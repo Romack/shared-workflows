e44eb7c14abc9fefe414894e591df6acf56d07a6
Chris Romack <chris.romack@invitationhomes.com>
2022-06-01 16:19:45 -0500

    test

--- .github/actions/get-heroku-application-name/dist/index.js
+++ .github/actions/get-heroku-application-name/dist/index.js
@@ -17169,18 +17169,20 @@ const REPOSITORY_METADATA_FILE = "./.repo-metadata-bad.yaml";
 const getHerokuApplicationName = (environment) => {
   const applicationName = github.context.payload.repository.name;
   const defaultHerokuApplicationName = `invh-${applicationName}-${environment}`;
-  console.log(`Application Name: ${applicationName}`);
-  console.log(`Default Heroku Application Name: ${defaultHerokuApplicationName}`);
+  console.info(`Application Name: ${applicationName}`);
+  console.info(`Default Heroku Application Name: ${defaultHerokuApplicationName}`);
 
   if (fs.existsSync(REPOSITORY_METADATA_FILE)) {
     const metadata = fs.readFileSync(REPOSITORY_METADATA_FILE, "utf-8");
     const parsed = yaml.parse(metadata);
     const herokuApplicationNameOverride = parsed["deployment"]["heroku-application-name"][environment];
-    console.log(`Heroku Application Name Override: ${herokuApplicationNameOverride}`);
+    console.info(`Heroku Application Name Override: ${herokuApplicationNameOverride}`);
 
     if (herokuApplicationNameOverride) {
       return herokuApplicationNameOverride;
     }
+  } else {
+    console.warning(".repo-metadata file missing for repository");
   }
 
   return defaultHerokuApplicationName;
@@ -17189,8 +17191,8 @@ const getHerokuApplicationName = (environment) => {
 try {
   const environment = core.getInput('environment');
   const herokuApplicationName = getHerokuApplicationName(environment);
-  console.log(`Environment: ${environment}`);
-  console.log(`Heroku Application Name: ${herokuApplicationName}`);
+  console.info(`Environment: ${environment}`);
+  console.info(`Heroku Application Name: ${herokuApplicationName}`);
 
   core.setOutput("heroku_application_name", "feenix-dev");
 } catch (error) {
