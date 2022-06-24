0f9de32c2e4a7975bbe9d555ff33af2b211fe26a
Chris Romack <chris.romack@invitationhomes.com>
2022-06-24 14:45:40 -0500

    some more

--- .github/actions/create-github-tag/index.js
+++ .github/actions/create-github-tag/index.js
@@ -3,22 +3,23 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 try {
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-
   const packageJson = fs.readFileSync("./package.json", "utf-8");
   const parsed = JSON.parse(packageJson);
+  const shortSha = github.context.sha.substr(0, 7);
+  const tagName = `${parsed.version}-${shortSha}`;
+
   console.log(`Version: ${parsed.version}`);
-  console.log(`GitHub Context: ${JSON.stringify(github.context)}`);
+  console.log(`Sha: ${github.context.sha}`);
+  console.log(`Creating tag: ${tagName}`);
 
   // const applicationName = github.context.payload.repository.name;
-
   // const octoKit = github.getOctokit(GITHUB_TOKEN);
   // const environment = core.getInput('environment');
   // const herokuApplicationName = getHerokuApplicationName(environment);
   // console.info(`Environment: ${environment}`);
   // console.info(`Heroku Application Name: ${herokuApplicationName}`);
 
-  core.setOutput("release_tag", "my-test");
+  core.setOutput("release_tag", tagName);
 } catch (error) {
   core.setFailed(error.message);
 }
