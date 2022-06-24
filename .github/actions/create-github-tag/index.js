cdc16f1f35528ec4ac348051008898a412ad0868
Chris Romack <chris.romack@invitationhomes.com>
2022-06-24 14:29:11 -0500

    feat: add the create-github-tag action

---
+++ .github/actions/create-github-tag/index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+try {
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+  const packageJson = fs.readFileSync("./package.json", "utf-8");
+  const parsed = JSON.parse(packageJson);
+  console.log(`Version: ${parsed.version}`);
+
+  // const octoKit = github.getOctokit(GITHUB_TOKEN);
+  // const environment = core.getInput('environment');
+  // const herokuApplicationName = getHerokuApplicationName(environment);
+  // console.info(`Environment: ${environment}`);
+  // console.info(`Heroku Application Name: ${herokuApplicationName}`);
+
+  core.setOutput("release_tag", "my-test");
+} catch (error) {
+  core.setFailed(error.message);
+}
+
