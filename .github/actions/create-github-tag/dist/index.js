f30f9c77659894f1561dd8364b3cee4028b55083
Chris Romack <chris.romack@invitationhomes.com>
2022-06-27 14:15:52 -0500

    more

--- .github/actions/create-github-tag/dist/index.js
+++ .github/actions/create-github-tag/dist/index.js
@@ -8922,39 +8922,41 @@ const github = __nccwpck_require__(5438);
 const fs = __nccwpck_require__(5747);
 
 const run = async () => {
-  try {
-    const githubToken = core.getInput('github-token');
-    const octokit = github.getOctokit(githubToken);
+  const githubToken = core.getInput('github-token');
+  console.log(`Token: ${githubToken}`);
+  const octokit = github.getOctokit(githubToken);
 
-    const packageJson = fs.readFileSync("./package.json", "utf-8");
-    const parsed = JSON.parse(packageJson);
-    const shortSha = github.context.sha.substr(0, 7);
-    const tagName = `${parsed.version}-${shortSha}`;
+  const packageJson = fs.readFileSync("./package.json", "utf-8");
+  const parsed = JSON.parse(packageJson);
+  const shortSha = github.context.sha.substr(0, 7);
+  const tagName = `${parsed.version}-${shortSha}`;
 
-    core.info(`Creating tag: ${tagName} for commit: ${github.context.sha}`);
-    const annotatedTag = await octokit.git.createTag({
-      ...github.context.repo,
-      tag: tagName,
-      message: tagName,
-      object: github.context.sha,
-      type: 'commit',
-    });
+  const newIssue = await octokit.rest.issues.create({
+    ...github.context.repo,
+    title: 'New issue!',
+    body: 'Hello Universe!'
+  });
 
-    core.info(`AnnotatedTag tag sha: ${annotatedTag.data.sha}`);
-    await octokit.git.createRef({
-      ...github.context.repo,
-      ref: `refs/tags/${tagName}`,
-      sha: annotatedTag.data.sha
-    });
+  core.info(`Creating tag: ${tagName} for commit: ${github.context.sha}`);
+  const annotatedTag = await octokit.git.createTag({
+    ...github.context.repo,
+    tag: tagName,
+    message: tagName,
+    object: github.context.sha,
+    type: 'commit',
+  });
 
-    core.setOutput("release_tag", tagName);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  core.info(`AnnotatedTag tag sha: ${annotatedTag.data.sha}`);
+  await octokit.git.createRef({
+    ...github.context.repo,
+    ref: `refs/tags/${tagName}`,
+    sha: annotatedTag.data.sha
+  });
+
+  core.setOutput("release_tag", tagName);
 }
 
-run();
-
+run().catch(error => core.setFailed(error.message));
 
 })();
 
