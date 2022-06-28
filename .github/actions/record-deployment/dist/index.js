99df24c47702e3387cd7757682d237caa4778c70
Chris Romack <chris.romack@invitationhomes.com>
2022-06-27 19:50:32 -0500

    more

--- .github/actions/record-deployment/dist/index.js
+++ .github/actions/record-deployment/dist/index.js
@@ -13919,15 +13919,7 @@ const run = async () => {
   const apiToken = core.getInput('api-token');
   const environment = core.getInput('environment');
   const version = core.getInput('version');
-
-  // 2021-09-15T18:03:09.967342Z
-  const timestamp = new Date().toISOString();
-  core.info(`Recording deployment...`);
-  core.info(`Repository Name: ${github.context.payload.repository.name}`);
-  core.info(`SHA: ${github.context.sha}`);
-  core.info(`Version: ${version}`);
-  core.info(`Environment: ${environment}`);
-  core.info(`Timestamp: ${timestamp}`);
+  core.info(`Recording deployment of ${version} to ${environment}`);
 
   await axios__WEBPACK_IMPORTED_MODULE_0___default().post(
     'https://api-dev.invitationhomes.com/ci-cd/v1/deployments',
--- .github/actions/record-deployment/index.js
+++ .github/actions/record-deployment/index.js
@@ -6,15 +6,7 @@ const run = async () => {
   const apiToken = core.getInput('api-token');
   const environment = core.getInput('environment');
   const version = core.getInput('version');
-
-  // 2021-09-15T18:03:09.967342Z
-  const timestamp = new Date().toISOString();
-  core.info(`Recording deployment...`);
-  core.info(`Repository Name: ${github.context.payload.repository.name}`);
-  core.info(`SHA: ${github.context.sha}`);
-  core.info(`Version: ${version}`);
-  core.info(`Environment: ${environment}`);
-  core.info(`Timestamp: ${timestamp}`);
+  core.info(`Recording deployment of ${version} to ${environment}`);
 
   await axios.post(
     'https://api-dev.invitationhomes.com/ci-cd/v1/deployments',
