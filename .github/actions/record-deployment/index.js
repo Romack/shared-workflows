f4f9f7135978100e5e8004659afd4b9a87044d26
Chris Romack <chris.romack@invitationhomes.com>
2022-06-27 19:24:12 -0500

    record deployment

---
+++ .github/actions/record-deployment/index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const run = async (application, environment, version) => {
+  const apiToken = core.getInput('api-token');
+  // 2021-09-15T18:03:09.967342Z
+  const timestamp = new Date().toISOString();
+  core.info(`Recording deployment...`);
+  core.info(`Repository Name: ${github.context.payload.repository.name}`);
+  core.info(`SHA: ${github.context.sha}`);
+  core.info(`Version: ${version}`);
+  core.info(`Environment: ${environment}`);
+  core.info(`Timestamp: ${timestamp}`);
+
+  await fetch(
+    "https://api-dev.invitationhomes.com/ci-cd/v1/deployments",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        application,
+        environment,
+        version,
+        commit: github.context.sha,
+        timestamp: new Date().toISOString(),
+        isSuccessful: 'true'
+      }),
+      headers: new Headers({ Authorization: `Bearer ${apiToken}` }),
+    });
+}
+
+run().catch(error => core.setFailed(error.message));
